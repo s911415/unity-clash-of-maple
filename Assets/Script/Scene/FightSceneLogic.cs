1f95acee1b1db9b88768911f4e772f92f8f49f3f
WeiChun <weichun@csie.tw>
2017-11-18 23:41:52 +0800

    Add Scene

---
+++ Assets/Script/Scene/FightSceneLogic.cs
@@ -0,0 +1,15 @@
+﻿using NTUT.CSIE.GameDev.Game;
+using NTUT.CSIE.GameDev.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NTUT.CSIE.GameDev.Scene
+{
+    public class FightSceneLogic : BasicSceneLogic
+    {
+        public Canvas _canvas;
+    }
+}
