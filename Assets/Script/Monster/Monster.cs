7a77cfc6ea50d52af6aeea889a66891207f998e7
WeiChun <weichun@csie.tw>
2017-11-26 16:40:48 +0800

    Add monster class

---
+++ Assets/Script/Monster/Monster.cs
@@ -0,0 +1,47 @@
+﻿using NTUT.CSIE.GameDev.Component;
+using NTUT.CSIE.GameDev.Game;
+using NTUT.CSIE.GameDev.Helper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace NTUT.CSIE.GameDev.Monster
+{
+    [Serializable]
+    public class Monster : CommonObject, IHurtable
+    {
+        public int id;
+        [SerializeField]
+        private Info _info;
+        [SerializeField]
+        private int _hp;
+
+        public enum Action
+        {
+            Idle, Walk, Attack, Die
+        }
+
+        public Action action = Action.Idle;
+
+        public int MAX_HP => _info.MaxHP;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _info = Manager.MonsterInfoCollection[id];
+            _hp = MAX_HP;
+        }
+
+        public void Damage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Recovery(int recover)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
