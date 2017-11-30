cf39a899937e649d37e4791c3733c16a8eb65221
WeiChun <weichun@csie.tw>
2017-11-30 15:45:08 +0800

    修改Grid Column的順序

---
+++ Assets/Script/Component/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NTUT.CSIE.GameDev.Component
+{
+    [Serializable]
+    public struct Point
+    {
+        [SerializeField]
+        private int _x, _y;
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X => _x;
+        public int Y => _y;
+    }
+}
